4a6fa20c16c20afc4de37b5eccc9b7903af554e2
Daniel_Kohout <daniel.kohout@tul.cz>
2024-03-07 13:07:32 +0100

    adding project

---
+++ first_try/src/test/java/cz/tul/mysti2024/cv/StinApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.tul.mysti2024.cv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
